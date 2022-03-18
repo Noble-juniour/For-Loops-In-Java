752e40b84938816cd7013a17e02ba561d44df83e
kevin <kevinindangasi1@gmail.com>
2022-03-18 16:52:37 +0300

    For Loops

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*int i = 1 ;
+    while (i <= 100){
+        System.out.println(i);
+        i++;*/
+    for(int i = 1; i <= 100; i++ )
+    {
+        System.out.println(i);
+    }
+    }
+}
